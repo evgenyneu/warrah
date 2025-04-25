1a71a2b74f6f080a23d52e37e36652d357aaea81
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-04-25 11:52:32 +0000

    add language map

---
+++ src/languages/comment_config.rs
@@ -0,0 +1,4 @@
+pub struct CommentConfig {
+    pub single_line: Vec<String>,
+    pub multi_line: Vec<(String, String)>,
+}
--- src/languages/mod.rs
+++ src/languages/mod.rs
@@ -1,2 +1,4 @@
+pub mod comment_config;
 pub mod docs_parse;
 pub mod language;
+pub mod language_map;
