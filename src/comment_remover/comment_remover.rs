8e38ffcad03b1fd7c15ff8ad33880d1d2965b6c6
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-05-04 09:42:33 +1000

    Simplify language detected and make it faster

--- src/comment_remover/comment_remover.rs
+++ src/comment_remover/comment_remover.rs
@@ -1,9 +1,8 @@
 // use super::remove_all_comments::remove_all_comments;
-use crate::languages::comment_config::CommentConfig;
 
 /// Removes comments from the content based on the comment configuration.
 /// Returns the content with comments removed.
-pub fn remove_comments(content: String, comments: &CommentConfig) -> String {
+pub fn remove_comments(content: String) -> String {
     // let content = remove_all_comments(&content, comments.single_line, comments.multi_line);
     content
 }
