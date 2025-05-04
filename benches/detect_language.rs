14b39d546c7c21f26dd82ef1d9198964dc8396c3
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-05-04 10:43:21 +1000

    Benchmark

--- benches/detect_language.rs
+++ benches/detect_language.rs
@@ -4,53 +4,45 @@ use warrah::languages::language_maps::{
     get_comments_by_language, get_language_by_extension, get_language_by_filename,
 };
 
-fn generate_test_paths() -> Vec<PathBuf> {
-    // Create a diverse set of test paths covering different languages
+fn generate_test_paths() -> Vec<String> {
     vec![
-        PathBuf::from("src/main.rs"),     // Rust
-        PathBuf::from("src/main.py"),     // Python
-        PathBuf::from("src/main.js"),     // JavaScript
-        PathBuf::from("src/main.ts"),     // TypeScript
-        PathBuf::from("src/main.java"),   // Java
-        PathBuf::from("src/main.cpp"),    // C++
-        PathBuf::from("src/main.c"),      // C
-        PathBuf::from("src/main.go"),     // Go
-        PathBuf::from("src/main.swift"),  // Swift
-        PathBuf::from("src/main.rb"),     // Ruby
-        PathBuf::from("src/main.php"),    // PHP
-        PathBuf::from("src/main.cs"),     // C#
-        PathBuf::from("src/main.dart"),   // Dart
-        PathBuf::from("src/main.kt"),     // Kotlin
-        PathBuf::from("src/main.scala"),  // Scala
-        PathBuf::from("src/main.groovy"), // Groovy
-        PathBuf::from("src/main.hs"),     // Haskell
-        PathBuf::from("src/main.fs"),     // F#
-        PathBuf::from("src/main.ex"),     // Elixir
-        PathBuf::from("src/main.exs"),    // Elixir
-        PathBuf::from("src/main.erl"),    // Erlang
-        PathBuf::from("src/main.clj"),    // Clojure
-        PathBuf::from("src/main.lisp"),   // Lisp
-        PathBuf::from("src/main.scm"),    // Scheme
-        PathBuf::from("src/main.zig"),    // Zig
-        PathBuf::from("src/main.v"),      // Verilog
-        PathBuf::from("src/main.sv"),     // SystemVerilog
-        PathBuf::from("src/main.vb"),     // VB
-        PathBuf::from("src/main.vbs"),    // VBScript
-        PathBuf::from("src/main.zig"),    // Zig
+        ".rs".to_string(),     // Rust
+        ".py".to_string(),     // Python
+        ".js".to_string(),     // JavaScript
+        ".ts".to_string(),     // TypeScript
+        ".java".to_string(),   // Java
+        ".cpp".to_string(),    // C++
+        ".c".to_string(),      // C
+        ".go".to_string(),     // Go
+        ".swift".to_string(),  // Swift
+        ".rb".to_string(),     // Ruby
+        ".php".to_string(),    // PHP
+        ".cs".to_string(),     // C#
+        ".dart".to_string(),   // Dart
+        ".kt".to_string(),     // Kotlin
+        ".scala".to_string(),  // Scala
+        ".groovy".to_string(), // Groovy
+        ".hs".to_string(),     // Haskell
+        ".fs".to_string(),     // F#
+        ".ex".to_string(),     // Elixir
+        ".exs".to_string(),    // Elixir
+        ".erl".to_string(),    // Erlang
+        ".clj".to_string(),    // Clojure
+        ".lisp".to_string(),   // Lisp
+        ".scm".to_string(),    // Scheme
+        ".zig".to_string(),    // Zig
+        ".v".to_string(),      // Verilog
+        ".sv".to_string(),     // SystemVerilog
+        ".vb".to_string(),     // VB
+        ".vbs".to_string(),    // VBScript
+        ".zig".to_string(),    // Zig
     ]
 }
 
-fn detect_language(path: &PathBuf) -> Option<&'static str> {
-    // First try to get language by filename
-    if let Some(lang) = get_language_by_filename(path.file_name()?.to_str()?) {
-        return Some(lang);
-    }
-
+fn detect_language(extension: &str) -> Option<&'static str> {
     // Then try to get language by extension
-    if let Some(ext) = path.extension() {
-        if let Some(lang) = get_language_by_extension(&format!(".{}", ext.to_str()?)) {
-            return Some(lang);
-        }
+    if let Some(lang) = get_language_by_extension(extension) {
+        return Some(lang);
     }
 
     None
